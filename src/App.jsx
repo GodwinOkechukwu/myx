450be39a4463085c98e9024080a523151b5832a1
kenesongodwin@gmail.com <kenesongodwin@gmail.com>
2025-08-15 00:28:58 +0100

    ui fix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyxelliaDashboard from "./components/MyxelliaDashboard";
+
+function App() {
+  return (
+    <div className="">
+      <MyxelliaDashboard />
+    </div>
+  );
+}
+
+export default App;
